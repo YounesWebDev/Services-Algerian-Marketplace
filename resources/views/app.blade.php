d77eef82274b0e2967d00c77100b364c75f4bcea
Younes <webdevyns@gmail.com>
2026-01-19 15:38:36 +0100

    fix: do not load Inertia pages as Vite entries

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -40,8 +40,10 @@
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
         @viteReactRefresh
-        @vite(['resources/js/app.tsx', "resources/js/Pages/{$page['component']}.tsx"])
+            @vite(['resources/css/app.css', 'resources/js/app.tsx'])
         @inertiaHead
+
+
     </head>
     <body class="font-sans antialiased">
         @inertia
