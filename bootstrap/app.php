2f5d53a937d83bd7c53715e26c9820758c7926fc
Younes <webdevyns@gmail.com>
2026-01-21 10:38:35 +0100

    fix errorr

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,9 +14,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        if (app()->environment('testing')) {
-        $middleware->disable(VerifyCsrfToken::class);
-    }
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
         $middleware->alias([
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,8 +4,19 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Foundation\Http\Middleware\ValidateCsrfToken;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication, RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // ✅ Disable CSRF for tests (Laravel 12 uses ValidateCsrfToken in the web group)
+        $this->withoutMiddleware(ValidateCsrfToken::class);
+        $this->withoutMiddleware(VerifyCsrfToken::class);
+    }
 }
